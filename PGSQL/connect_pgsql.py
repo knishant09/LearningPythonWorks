68f2e9c37c9d48e10e51b6d7782c3de77920caaf
knishant09 <k.nishant09@gmail.com>
2019-02-18 17:16:05 +0530

    PSSQL

---
+++ PGSQL/connect_pgsql.py
@@ -0,0 +1,41 @@
+import psycopg2
+from PGSQL.config import config
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+        print(params)
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        cur.execute("SELECT DISTINCT pool_name from pool_usage")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
